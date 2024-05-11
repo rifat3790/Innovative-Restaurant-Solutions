7689c22c776e4ef8319de6ec57b3dd15ffbe8ea6
rifat3790 <mdrifayethosse@gmailcom>
2024-05-11 23:18:11 +0600

    added footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,8 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
 
 const Main = () => {
     return (
         <div>
-            
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
