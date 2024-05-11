1fcd667aae55bdeaf1349a4644f0bcdd43d21a62
rifat3790 <mdrifayethosse@gmailcom>
2024-05-11 23:49:35 +0600

    added banner

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
     return (
       <>
-        <div className="navbar bg-base-100">
+        <div className="navbar max-w-screen-xl fixed z-10 bg-opacity-30 bg-black text-white">
           <div className="navbar-start">
             <div className="dropdown">
               <div
