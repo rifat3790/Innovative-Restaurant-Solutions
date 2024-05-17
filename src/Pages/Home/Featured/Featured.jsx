f3848d58b5d57fbe42fd580a0fcba44d58029de2
rifat3790 <mdrifayethosse@gmailcom>
2024-05-17 23:19:25 +0600

    added rating

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -4,10 +4,10 @@ import './Featured.css'
 
 const Featured = () => {
     return (
-        <div className="featured-item text-white">
+        <div className="featured-item bg-fixed text-white pt-8 my-20">
             <SectionTitle subHeading="Check it out" heading="Featured Item"></SectionTitle>
 
-            <div className="md:flex justify-center items-center py-8 px-16">
+            <div className="md:flex justify-center items-center bg-slate-500 bg-opacity-40 pb-20 pt-12 px-36">
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
@@ -15,7 +15,7 @@ const Featured = () => {
                     <p>May 17, 2024</p>
                     <p className="uppercase">Where can i get some?</p>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo, laborum neque. Eos error labore illo consequatur qui tenetur dicta sed provident quibusdam! Vero necessitatibus omnis, cumque, praesentium eveniet ducimus quae voluptas porro reiciendis modi esse quis ullam velit maiores, totam atque fugiat eos delectus exercitationem beatae ipsum? Quia, quaerat ipsa?</p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
                 </div>
             </div>
         </div>
---
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,40 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+
+import "@smastrom/react-rating/style.css";
+
+const Testimonials = () => {
+    const [reviews, setReviews] = useState([]);
+    useEffect( ()=> {
+        fetch('reviews.json')
+        .then(res => res.json())
+        .then(data => setReviews(data))
+    },[])
+    return (
+      <section className="my-20">
+        <SectionTitle
+          subHeading="What Our Client Say"
+          heading="Testimonial"></SectionTitle>
+
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          {reviews.map((review) => (
+            <SwiperSlide key={review._id}>
+              <div className="m-24">
+                <Rating style={{ maxWidth: 180 }} value={review.rating} readOnly />
+                <p>{review.details}</p>
+                <h3 className="text-2xl text-orange-400">{review.name}</h3>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </section>
+    );
+};
+
+export default Testimonials;
